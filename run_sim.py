879958bf59b7fac081391feb339d542db99155b4
wisemanp <philip@wisemen.co.uk>
2020-06-18 17:22:29 +0100

    Add survey area

--- run_sim.py
+++ run_sim.py
@@ -30,6 +30,8 @@ def parser():
     parser.add_argument('--r_max',help='Max distance or redshift to sample',dest='Spatial.r_max',required=False)
     parser.add_argument('--Lambda',help='Reference to spatial density of galaxies/SNe: [D08_z]',dest='Spatial.Lambda',required=False)
     parser.add_argument('--delta',help='Exponent of redshift evolution of spatial distribution',dest='Spatial.cosmo.delta',required=False)
+    parser.add_argument('--n_fields',help='Exponent of redshift evolution of spatial distribution',dest='Spatial.des.n_fields',required=False)
+
     return parser.parse_args()
 
 def main():
@@ -40,7 +42,7 @@ def main():
     sim = ZPowerCosmoSchechterSim(
         Lstar=config['Luminosity']['Schechter']['Lstar'].get(float)*c.Lsun,
         alpha=config['Luminosity']['Schechter']['alpha'].get(float)+1,
-        Lambda=getattr(c,config['Spatial']['Lambda'].get(str)),
+        Lambda=getattr(c,config['Spatial']['Lambda'].get(str))*config['Spatial']['des']['n_fields']*c.des_area_frac,
         delta=config['Spatial']['cosmo']['delta'].get(float),
         r_max=config['Spatial']['r_max'].get(float))
     print("Going to synthesise a population with these parameters: ")
