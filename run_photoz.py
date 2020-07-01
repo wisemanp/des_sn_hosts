afacb79a5f12b4f2a331464d07d6b087d75b8b1b
wisemanp <philip@wisemen.co.uk>
2020-07-01 10:25:53 +0100

    Add multiprocessing

--- run_photoz.py
+++ run_photoz.py
@@ -3,6 +3,9 @@ import pandas as pd
 import argparse
 import os
 import yaml
+import tqdm
+from tqdm.contrib.concurrent import process_map
+
 from des_sn_hosts.functions import photoz
 from des_sn_hosts.utils.utils import get_good_des_chips
 def parser():
@@ -20,18 +23,37 @@ def parser():
     else:
         args.chips =args.chips.split(',')
     return args
+
+def pz_worker(worker_args):
+    args = worker_args[0]
+    f = worker_args[1]
+    ch = worker_args[2]
+    cat_fn = os.path.join(config['des_root'],
+        '5yr_stacks/MY%s/SN-%s/CAP/%s/%s_SN-%s_%s_obj_deep_v%s.cat'%(args.my,f,ch,args.my,f,ch,config['cat_version']))
+    args.input = cat_fn
+    args.output = os.path.join(config['des_root'],
+        '5yr_stacks/MY%s/SN-%s/CAP/%s/'%(args.my,f,ch),
+        '%s_SN-%s_%s_%s_%s_%s'%(args.my,f,ch,config['cat_version'],config['params']['Z_MAX'],config['params']['Z_STEP']))
+    photoz.main(args)
+
+def multi_pz(args,f):
+    pool_size = multiprocessing.cpu_count()*2
+    pool = multiprocessing.Pool(processes=pool_size,
+                                maxtasksperchild=2,
+                                )
+    worker_args = []
+    for ch in args.chips:
+        worker_args.append([args,ch])
+    for _ in tqdm.tqdm(pool.imap_unordered(pz_worker,worker_args),total=len(args.chips)):
+        pass
+    pool.close()
+    pool.join()
 def main():
     args = parser()
     config = yaml.load(open(args.config))
     args.config = config
     for f in args.fields:
-        for ch in args.chips:
-            cat_fn = os.path.join(config['des_root'],
-                '5yr_stacks/MY%s/SN-%s/CAP/%s/%s_SN-%s_%s_obj_deep_v%s.cat'%(args.my,f,ch,args.my,f,ch,config['cat_version']))
-            args.input = cat_fn
-            args.output = os.path.join(config['des_root'],
-                '5yr_stacks/MY%s/SN-%s/CAP/%s/'%(args.my,f,ch),
-                '%s_SN-%s_%s_%s_%s_%s'%(args.my,f,ch,config['cat_version'],config['params']['Z_MAX'],config['params']['Z_STEP']))
-            photoz.main(args)
+        multi_pz(args,f)
+
 if __name__=="__main__":
     main()
