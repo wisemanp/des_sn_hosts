2857c6cc779e8a77b41180f70c20328a8b423448
wisemanp <philip@wisemen.co.uk>
2020-07-08 14:21:20 +0100

    Fixing err col naming

--- rates/rates_utils.py
+++ rates/rates_utils.py
@@ -9,7 +9,7 @@ def sample_sn_masses(df,model_dir,mass_col='log_m',mass_err_col='logm_err',sfr_c
     nobs=len(detections)
     fit = model_gen.sampling(data=dict(n_obs=nobs,
                                   x_obs =detections[mass_col].values,
-                                  x_err =detections[err_col].values+0.001),
+                                  x_err =detections[mass_err_col].values+0.001),
                         seed=seed,algorithm='Fixed_param',iter=n_iter,chains=1,verbose=True)
     chain = fit.extract()
     df_bootstrapped = pd.DataFrame(chain['x_sim'].T)
@@ -30,7 +30,7 @@ def sample_field_masses(df,model_dir,mass_col='log_m',mass_err_col='logm_err',sf
     nobs=len(detections)
     fit = model_gen.sampling(data=dict(n_obs=nobs,
                                   x_obs =detections[mass_col].values,
-                                  x_err =detections[err_col].values+0.001),
+                                  x_err =detections[mass_err_col].values+0.001),
                         seed=seed,algorithm='Fixed_param',iter=n_iter,chains=1,verbose=True)
     chain = fit.extract()
     df_bootstrapped = pd.DataFrame(chain['x_sim'].T)
