431586153f0bdeef32ba3d31261458ca0938ddb4
wisemanp <philip@wisemen.co.uk>
2020-07-15 12:46:40 +0100

    Correct column renaming for fixed z

--- functions/photoz.py
+++ functions/photoz.py
@@ -111,13 +111,13 @@ def prep_eazy_data(allgals,out_fn):
 def prep_eazy_data_fixed_z(allgals,out_fn):
     print('Working path: %s'%out_fn)
 
-    allgals = allgals[['ID','z_phot',
+    allgals = allgals[['id','z_phot',
                         'F294', 'F295', 'F296', 'F297', 'E294', 'E295',
                               'E296', 'E297']]
 
     print ('Going to work on %s galaxies!'%len(allgals))
 
-    for_eazy = allgals.rename(columns={'ID':'id',
+    for_eazy = allgals.rename(columns={
                                          'z_phot':'z_spec'})
 
 
@@ -204,103 +204,105 @@ def main(args):
     print('Attempting to read %s'%args.input)
     if args.input.split('.')[-1]=='cat':
         df = pd.read_csv(args.input)
+
+
+        df = df.rename(index=str,columns={'Unnamed: 0':'ID',
+                                          'X_WORLD':'RA','Y_WORLD':'DEC',
+                                            'MY':'SEASON',
+                                            'ANGSEP':'SEPARATION',
+                                           'z':'SPECZ',
+                                           'ez':'SPECZ_ERR',
+                                           'source':'SPECZ_CATALOG',
+                                           'flag':'SPECZ_FLAG',
+                                           #'objtype_ozdes':'OBJTYPE_OZDES',
+                                           #'transtype_ozdes':'TRANSTYPE_OZDES',
+                                           'MAG_APER_g':'MAG_APER_4_G',
+                                           'MAG_APER_r':'MAG_APER_4_R',
+                                           'MAG_APER_i':'MAG_APER_4_I',
+                                           'MAG_APER_z':'MAG_APER_4_Z',
+                                           'MAGERR_APER_g':'MAGERR_APER_4_G',
+                                           'MAGERR_APER_r':'MAGERR_APER_4_R',
+                                           'MAGERR_APER_i':'MAGERR_APER_4_I',
+                                           'MAGERR_APER_z':'MAGERR_APER_4_Z',
+                                           'MAGERR_SYST_APER_g':'MAGERR_SYST_APER_4_G',
+                                           'MAGERR_SYST_APER_r':'MAGERR_SYST_APER_4_R',
+                                           'MAGERR_SYST_APER_i':'MAGERR_SYST_APER_4_I',
+                                           'MAGERR_SYST_APER_z':'MAGERR_SYST_APER_4_Z',
+                                           'MAGERR_STATSYST_APER_g':'MAGERR_STATSYST_APER_4_G',
+                                           'MAGERR_STATSYST_APER_r':'MAGERR_STATSYST_APER_4_R',
+                                           'MAGERR_STATSYST_APER_i':'MAGERR_STATSYST_APER_4_I',
+                                           'MAGERR_STATSYST_APER_z':'MAGERR_STATSYST_APER_4_Z',
+                                           'MAG_AUTO_g':'MAG_AUTO_G',
+                                           'MAG_AUTO_r':'MAG_AUTO_R',
+                                           'MAG_AUTO_i':'MAG_AUTO_I',
+                                           'MAG_AUTO_z':'MAG_AUTO_Z',
+                                           'MAGERR_AUTO_g':'MAGERR_AUTO_G',
+                                           'MAGERR_AUTO_r':'MAGERR_AUTO_R',
+                                           'MAGERR_AUTO_i':'MAGERR_AUTO_I',
+                                           'MAGERR_AUTO_z':'MAGERR_AUTO_Z',
+                                           'MAGERR_SYST_AUTO_g':'MAGERR_SYST_AUTO_G',
+                                           'MAGERR_SYST_AUTO_r':'MAGERR_SYST_AUTO_R',
+                                           'MAGERR_SYST_AUTO_i':'MAGERR_SYST_AUTO_I',
+                                           'MAGERR_SYST_AUTO_z':'MAGERR_SYST_AUTO_Z',
+                                           'MAGERR_STATSYST_AUTO_g':'MAGERR_STATSYST_AUTO_G',
+                                           'MAGERR_STATSYST_AUTO_r':'MAGERR_STATSYST_AUTO_R',
+                                           'MAGERR_STATSYST_AUTO_i':'MAGERR_STATSYST_AUTO_I',
+                                           'MAGERR_STATSYST_AUTO_z':'MAGERR_STATSYST_AUTO_Z',
+                                           'FLUX_AUTO_g':'FLUX_AUTO_G',
+                                            'FLUX_AUTO_r':'FLUX_AUTO_R',
+                                            'FLUX_AUTO_i':'FLUX_AUTO_I',
+                                            'FLUX_AUTO_z':'FLUX_AUTO_Z',
+                                           'FLUXERR_AUTO_g':'FLUXERR_AUTO_G',
+                                            'FLUXERR_AUTO_r':'FLUXERR_AUTO_R',
+                                            'FLUXERR_AUTO_i':'FLUXERR_AUTO_I',
+                                            'FLUXERR_AUTO_z':'FLUXERR_AUTO_Z',
+                                           'FLUX_APER_g':'FLUX_APER_4_G',
+                                            'FLUX_APER_r':'FLUX_APER_4_R',
+                                            'FLUX_APER_i':'FLUX_APER_4_I',
+                                            'FLUX_APER_z':'FLUX_APER_4_Z',
+                                            'FLUXERR_APER_g':'FLUXERR_APER_4_G',
+                                            'FLUXERR_APER_r':'FLUXERR_APER_4_R',
+                                            'FLUXERR_APER_i':'FLUXERR_APER_4_I',
+                                            'FLUXERR_APER_z':'FLUXERR_APER_4_Z',
+                                           'CLASS_STAR_g':'CLASS_STAR_G',
+                                           'CLASS_STAR_r':'CLASS_STAR_R',
+                                           'CLASS_STAR_i':'CLASS_STAR_I',
+                                           'CLASS_STAR_z':'CLASS_STAR_Z',
+                                           'MAG_ZEROPOINT_g':'MAG_ZEROPOINT_G',
+                                           'MAG_ZEROPOINT_r':'MAG_ZEROPOINT_R',
+                                           'MAG_ZEROPOINT_i':'MAG_ZEROPOINT_I',
+                                           'MAG_ZEROPOINT_z':'MAG_ZEROPOINT_Z',
+                                           'LIMMAG_g':'LIMMAG_G',
+                                           'LIMMAG_r':'LIMMAG_R',
+                                           'LIMMAG_i':'LIMMAG_I',
+                                           'LIMMAG_z':'LIMMAG_Z',
+                                           'LIMFLUX_g':'LIMFLUX_G',
+                                           'LIMFLUX_r':'LIMFLUX_R',
+                                           'LIMFLUX_i':'LIMFLUX_I',
+                                           'LIMFLUX_z':'LIMFLUX_Z',
+                                           'MAG_ZEROPOINT_ERR_g':'MAG_ZEROPOINT_ERR_G',
+                                           'MAG_ZEROPOINT_ERR_r':'MAG_ZEROPOINT_ERR_R',
+                                           'MAG_ZEROPOINT_ERR_i':'MAG_ZEROPOINT_ERR_I',
+                                           'MAG_ZEROPOINT_ERR_z':'MAG_ZEROPOINT_ERR_Z'})
+
+        df[pd.isna(df['MAGERR_STATSYST_AUTO_G'])]['MAGERR_AUTO_G'].unique()
+
+        df['Z_RANK'] = df['Z_RANK'].fillna(-9.999)
+
+        df['EDGE_FLAG'] = get_edge_flags(df.X_IMAGE.values,df.Y_IMAGE.values)
+
+        df = df[(df['EDGE_FLAG']==0)& (df['Z_RANK']<2)]
+        df = df[(df['CLASS_STAR_G']<0.5)&(df['CLASS_STAR_R']<0.5)&(df['CLASS_STAR_I']<0.5)&(df['CLASS_STAR_Z']<0.5)]
+        for b in ['G','R','I','Z']:
+            df.loc[df[df['MAGERR_AUTO_%s'%b]<0].index,'MAGERR_STATSYST_AUTO_%s'%b]=-1
+            df['MAGERR_STATSYST_AUTO_%s'%b].replace(np.NaN,-1)
+            df['MAGERR_STATSYST_AUTO_%s'%b] = df['MAGERR_STATSYST_AUTO_%s'%b].astype(float)
+
+        df.index = df.index.astype(int)
+        df['ID'] = df.index.values
+
     elif args.input.split('.')[-1]=='h5':
         df = pd.read_hdf(args.input,key='photozs')
-
-    df = df.rename(index=str,columns={'Unnamed: 0':'ID',
-                                      'X_WORLD':'RA','Y_WORLD':'DEC',
-                                        'MY':'SEASON',
-                                        'ANGSEP':'SEPARATION',
-                                       'z':'SPECZ',
-                                       'ez':'SPECZ_ERR',
-                                       'source':'SPECZ_CATALOG',
-                                       'flag':'SPECZ_FLAG',
-                                       #'objtype_ozdes':'OBJTYPE_OZDES',
-                                       #'transtype_ozdes':'TRANSTYPE_OZDES',
-                                       'MAG_APER_g':'MAG_APER_4_G',
-                                       'MAG_APER_r':'MAG_APER_4_R',
-                                       'MAG_APER_i':'MAG_APER_4_I',
-                                       'MAG_APER_z':'MAG_APER_4_Z',
-                                       'MAGERR_APER_g':'MAGERR_APER_4_G',
-                                       'MAGERR_APER_r':'MAGERR_APER_4_R',
-                                       'MAGERR_APER_i':'MAGERR_APER_4_I',
-                                       'MAGERR_APER_z':'MAGERR_APER_4_Z',
-                                       'MAGERR_SYST_APER_g':'MAGERR_SYST_APER_4_G',
-                                       'MAGERR_SYST_APER_r':'MAGERR_SYST_APER_4_R',
-                                       'MAGERR_SYST_APER_i':'MAGERR_SYST_APER_4_I',
-                                       'MAGERR_SYST_APER_z':'MAGERR_SYST_APER_4_Z',
-                                       'MAGERR_STATSYST_APER_g':'MAGERR_STATSYST_APER_4_G',
-                                       'MAGERR_STATSYST_APER_r':'MAGERR_STATSYST_APER_4_R',
-                                       'MAGERR_STATSYST_APER_i':'MAGERR_STATSYST_APER_4_I',
-                                       'MAGERR_STATSYST_APER_z':'MAGERR_STATSYST_APER_4_Z',
-                                       'MAG_AUTO_g':'MAG_AUTO_G',
-                                       'MAG_AUTO_r':'MAG_AUTO_R',
-                                       'MAG_AUTO_i':'MAG_AUTO_I',
-                                       'MAG_AUTO_z':'MAG_AUTO_Z',
-                                       'MAGERR_AUTO_g':'MAGERR_AUTO_G',
-                                       'MAGERR_AUTO_r':'MAGERR_AUTO_R',
-                                       'MAGERR_AUTO_i':'MAGERR_AUTO_I',
-                                       'MAGERR_AUTO_z':'MAGERR_AUTO_Z',
-                                       'MAGERR_SYST_AUTO_g':'MAGERR_SYST_AUTO_G',
-                                       'MAGERR_SYST_AUTO_r':'MAGERR_SYST_AUTO_R',
-                                       'MAGERR_SYST_AUTO_i':'MAGERR_SYST_AUTO_I',
-                                       'MAGERR_SYST_AUTO_z':'MAGERR_SYST_AUTO_Z',
-                                       'MAGERR_STATSYST_AUTO_g':'MAGERR_STATSYST_AUTO_G',
-                                       'MAGERR_STATSYST_AUTO_r':'MAGERR_STATSYST_AUTO_R',
-                                       'MAGERR_STATSYST_AUTO_i':'MAGERR_STATSYST_AUTO_I',
-                                       'MAGERR_STATSYST_AUTO_z':'MAGERR_STATSYST_AUTO_Z',
-                                       'FLUX_AUTO_g':'FLUX_AUTO_G',
-                                        'FLUX_AUTO_r':'FLUX_AUTO_R',
-                                        'FLUX_AUTO_i':'FLUX_AUTO_I',
-                                        'FLUX_AUTO_z':'FLUX_AUTO_Z',
-                                       'FLUXERR_AUTO_g':'FLUXERR_AUTO_G',
-                                        'FLUXERR_AUTO_r':'FLUXERR_AUTO_R',
-                                        'FLUXERR_AUTO_i':'FLUXERR_AUTO_I',
-                                        'FLUXERR_AUTO_z':'FLUXERR_AUTO_Z',
-                                       'FLUX_APER_g':'FLUX_APER_4_G',
-                                        'FLUX_APER_r':'FLUX_APER_4_R',
-                                        'FLUX_APER_i':'FLUX_APER_4_I',
-                                        'FLUX_APER_z':'FLUX_APER_4_Z',
-                                        'FLUXERR_APER_g':'FLUXERR_APER_4_G',
-                                        'FLUXERR_APER_r':'FLUXERR_APER_4_R',
-                                        'FLUXERR_APER_i':'FLUXERR_APER_4_I',
-                                        'FLUXERR_APER_z':'FLUXERR_APER_4_Z',
-                                       'CLASS_STAR_g':'CLASS_STAR_G',
-                                       'CLASS_STAR_r':'CLASS_STAR_R',
-                                       'CLASS_STAR_i':'CLASS_STAR_I',
-                                       'CLASS_STAR_z':'CLASS_STAR_Z',
-                                       'MAG_ZEROPOINT_g':'MAG_ZEROPOINT_G',
-                                       'MAG_ZEROPOINT_r':'MAG_ZEROPOINT_R',
-                                       'MAG_ZEROPOINT_i':'MAG_ZEROPOINT_I',
-                                       'MAG_ZEROPOINT_z':'MAG_ZEROPOINT_Z',
-                                       'LIMMAG_g':'LIMMAG_G',
-                                       'LIMMAG_r':'LIMMAG_R',
-                                       'LIMMAG_i':'LIMMAG_I',
-                                       'LIMMAG_z':'LIMMAG_Z',
-                                       'LIMFLUX_g':'LIMFLUX_G',
-                                       'LIMFLUX_r':'LIMFLUX_R',
-                                       'LIMFLUX_i':'LIMFLUX_I',
-                                       'LIMFLUX_z':'LIMFLUX_Z',
-                                       'MAG_ZEROPOINT_ERR_g':'MAG_ZEROPOINT_ERR_G',
-                                       'MAG_ZEROPOINT_ERR_r':'MAG_ZEROPOINT_ERR_R',
-                                       'MAG_ZEROPOINT_ERR_i':'MAG_ZEROPOINT_ERR_I',
-                                       'MAG_ZEROPOINT_ERR_z':'MAG_ZEROPOINT_ERR_Z'})
-
-    df[pd.isna(df['MAGERR_STATSYST_AUTO_G'])]['MAGERR_AUTO_G'].unique()
-
-    df['Z_RANK'] = df['Z_RANK'].fillna(-9.999)
-
-    df['EDGE_FLAG'] = get_edge_flags(df.X_IMAGE.values,df.Y_IMAGE.values)
-
-    df = df[(df['EDGE_FLAG']==0)& (df['Z_RANK']<2)]
-    df = df[(df['CLASS_STAR_G']<0.5)&(df['CLASS_STAR_R']<0.5)&(df['CLASS_STAR_I']<0.5)&(df['CLASS_STAR_Z']<0.5)]
-    for b in ['G','R','I','Z']:
-        df.loc[df[df['MAGERR_AUTO_%s'%b]<0].index,'MAGERR_STATSYST_AUTO_%s'%b]=-1
-        df['MAGERR_STATSYST_AUTO_%s'%b].replace(np.NaN,-1)
-        df['MAGERR_STATSYST_AUTO_%s'%b] = df['MAGERR_STATSYST_AUTO_%s'%b].astype(float)
-    
-    df.index = df.index.astype(int)
-    df['ID'] = df.index.values
     #df =df[df['SPECZ']>0]
     print('Sending to prep!')
     if not args.fixz:
