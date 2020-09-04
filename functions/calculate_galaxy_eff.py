560f57daa2ac51e5f8e3bd56449f611da7ade40e
wisemanp <philip@wisemen.co.uk>
2020-09-04 16:37:28 +0100

    Allow adjustable sample sizes

--- functions/calculate_galaxy_eff.py
+++ functions/calculate_galaxy_eff.py
@@ -19,10 +19,8 @@ class Completeness():
     def r_i(self,row):
         Z = row['mu']
         M = row['M_%s'%self.b]
-        #print(M,Z)
         Mlim_B = self.mlim_B - (Z-self.dZ)
         sel = self.test_df[(self.test_df['mu']<Z)&(self.test_df['mu']>Z-self.dZ)&(self.test_df['M_%s'%self.b]<M)&(self.test_df['M_%s'%self.b]>Mlim_B)]
-        #print('ri',len(sel))
         return len(sel)
 
     def n_i(self,row,mlim_star):
@@ -30,47 +28,35 @@ class Completeness():
         Mlim_star = mlim_star - Z
         Mlim_B = self.mlim_B - (Z-self.dZ)
         M = row['M_%s'%self.b]
-        #if M>Mlim_star:
-        #    sel = self.test_df[(self.test_df['mu']<Z)&(self.test_df['mu']>Z-self.dZ)&(self.test_df['M_%s'%self.b]<M)&(self.test_df['M_%s'%self.b]>Mlim_B)]
-        #print('%.2f < M <%.2f'%(Mlim_B,Mlim_star))
-        #else:
         sel = self.test_df[(self.test_df['mu']<Z)&(self.test_df['mu']>(Z-self.dZ))&(self.test_df['M_%s'%self.b]<Mlim_star)&(self.test_df['M_%s'%self.b]>Mlim_B)]
-        #print(len(sel))
-        #print('ni',len(sel))
         return len(sel)
 
     def q_i(self,row):
         Z = row['mu']
         M = row['M_%s'%self.b]
-
-        sel = self.df[(self.df['mu']<Z)&(self.df['M_%s'%self.b]>(M-self.dM))&(self.df['M_%s'%self.b]<M)]
+        Mlim_B = self.mlim_B - (Z-self.dZ)
+        sel = self.test_df[(self.df['mu']<Z)&(self.test_df['M_%s'%self.b]>(M-self.dM))&(self.test_df['M_%s'%self.b]<M)]
         return len(sel)
 
     def t_i(self,row,mlim_star):
         Z = row['mu']
         M = row['M_%s'%self.b]
         Mlim_B = self.mlim_B - Z
-        sel = self.df[(self.df['MAG_AUTO_%s'%self.b.capitalize()]<mlim_star)&(self.df['M_%s'%self.b]>(M-self.dM))&(self.df['M_%s'%self.b]<M)]
+        Zmax = mlim_star - M
+        sel = self.test_df[(self.df['mu']<Zmax)&(self.test_df['M_%s'%self.b]>(M-self.dM))&(self.test_df['M_%s'%self.b]<M)]
         return len(sel)
 
     def var_zeta_i(self,ni):
-        #print((ni -1),(ni+1),(ni - 1)/(ni + 1))
-
         return (1/12)*(ni - 1)/(ni + 1)
 
     def zeta_i(self,row,mlim_star):
-
         r_i = self.r_i(row)
         n_i = self.n_i(row,mlim_star)
-
         zeta = r_i/(n_i+1)
         var = self.var_zeta_i(n_i)
-        #print(r_i,n_i,zeta,var)
         row['z_i'] = zeta
         row['var_z_i'] = var
         return row
-
-
 
     def tau_i(self,row,mlim_star):
         t_i = self.t_i(row,mlim_star)
@@ -88,29 +74,24 @@ class Completeness():
         self.test_df['z_i'] = ''
         self.test_df['var_z_i'] = ''
         self.test_df = self.test_df.apply(lambda x: self.zeta_i(x,mlim_star),axis=1)
-        #print('zeta i',self.df['z_i'].values - 0.5)
         print(mlim_star)
-        #print('zeta i',self.df['z_i'].values )
-        #print('mean of zeta i',self.test_df['z_i'].mean())
-
-        #print('var zeta i',self.test_df['var_z_i'].values)
-        #print(np.sum(self.test_df['z_i'].fillna(0).values - 0.5))
         self.test_df['var_z_i'].fillna(1/12,inplace=True)
         good_var = np.abs(self.test_df['var_z_i'].values)
-
-        #print(len(good_var),len(good_var[good_var>0]))
-        #print(np.sum(good_var**0.5))
-        Tci = np.sum(self.test_df['z_i'].fillna(0).values - 0.5)/np.sum(good_var**0.5)
-        print('Tc_i',Tci)
-        return Tci
+        Tc = np.sum(self.test_df['z_i'].fillna(0).values - 0.5)/np.sum(good_var**0.5)
+        print('Tc',Tc)
+        return Tc
 
     def Tv(self,mlim_star):
-        self.df['t_i'] = ''
-        self.df['var_t_i'] = ''
-        self.df = self.df.apply(lambda x: self.tau_i(x,mlim_star),axis=1)
-        sigma_t_i = np.sum(self.df['t_i'] - 0.5)
-        sigma_var_i = np.sum(self.df['var_t_i'])
-        return sigma_t_i/(sigma_var_i**0.5)
+        self.test_df = self.df[self.df['MAG_AUTO_%s'%self.b.capitalize()]<mlim_star]
+        self.test_df['t_i'] = ''
+        self.test_df['var_t_i'] = ''
+        self.test_df = self.test_df.apply(lambda x: self.tau_i(x,mlim_star),axis=1)
+        print(mlim_star)
+        self.test_df['var_t_i'].fillna(1/12,inplace=True)
+        good_var = np.abs(self.test_df['var_t_i'].values)
+        Tv = np.sum(self.test_df['t_i'].fillna(0).values - 0.5)/np.sum(good_var**0.5)
+        print('Tv',Tv)
+        return Tv
 
 
 def estimate_Tc(C, mlim_stars):
@@ -119,6 +100,12 @@ def estimate_Tc(C, mlim_stars):
         Tcs.append(C.Tc(m))
     return Tcs
 
+def estimate_Tv(C, mlim_stars):
+    Tvs = []
+    for m in tqdm(mlim_stars):
+        Tvs.append(C.Tv(m))
+    return Tvs
+
 def main():
     if not sys.argv[1]:
         fname = '/media/data3/wiseman/des/coadding/results/deep/deep_X3_hostlib.h5'
@@ -126,19 +113,28 @@ def main():
         fname = sys.argv[1]
 
     hostlib = pd.read_hdf(fname,key='main')
+    if not sys.argv[2]:
+        n_sample = 50000
+    else:
+        n_sample = float(sys.argv[2])
 
-    C = Completeness(hostlib.sample(50000),dZ=0.05,dM=0.05)
+    if not sys.argv[3]:
+        dmdz = 0.05
+    else:
+        dmdz = float(sys.argv[3])
+    C = Completeness(hostlib.sample(n_sample),dZ=dmdz,dM=dmdz)
     mlim_stars = np.linspace(23,29,19)
     Tcs = estimate_Tc(C,mlim_stars)
+    Tvs = estimate_Tv(C,mlim_stars)
     f,ax=plt.subplots()
     ax.scatter(mlim_stars,Tcs,label='$T_C$')
-    #ax.scatter(mlim_stars,Tvs,label='$T_V$')
+    ax.scatter(mlim_stars,Tvs,label='$T_V$')
     ax.legend()
-    plt.savefig('/media/data3/wiseman/des/desdtd/figs/completeness')
+    plt.savefig('/media/data3/wiseman/des/desdtd/figs/completeness_%s_%s'%(n_sample,dmdz))
     arr = np.zeros((len(mlim_stars),2))
     arr[:,0] = mlim_stars
     arr[:,1] = Tcs
 
-    np.savetxt('/media/data3/wiseman/des/desdtd/completeness/SN-X3_50k_completeness.dat',arr)
+    np.savetxt('/media/data3/wiseman/des/desdtd/completeness/SN-X3_completeness_%s_%s.dat'%(n_sample,dmdz),arr)
 if __name__=="__main__":
     main()
