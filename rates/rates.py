1bf5a8f9fa286bac2982011f88b2b4950304a956
wisemanp <philip@wisemen.co.uk>
2020-10-15 16:14:36 +0100

    Fixing missing kwarg

--- rates/rates.py
+++ rates/rates.py
@@ -242,7 +242,7 @@ class Rates():
 
         if not savename:
             savename=self.config['rates_root']+'data/mcd_rates.h5'
-        iter_df = SN_G_MC(self.sn_samples_mass,self.field_samples_mass,n_samples=n_samples,mmin=mmin,mmax=mmax,mstep=mstep,savename=savename, weight_col_SN=weight_col_SN,weight_col_field=weight_col_field,key_ext=variable+'_%.2f'%mstep)
+        iter_df = SN_G_MC(self.sn_samples_mass,self.field_samples_mass,n_samples=n_samples,mmin=mmin,mmax=mmax,mstep=mstep,savename=savename, weight_col_SN=weight_col_SN,weight_col_field=weight_col_field,key_ext='%.2f'%mstep)
         self.sampled_rates_mass = iter_df
 
 
