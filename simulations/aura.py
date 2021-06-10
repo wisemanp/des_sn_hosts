5054c05886532740ed3394971cb995c0a44593fe
wisemanp <pacelweb@gmail.com>
2021-06-10 11:58:12 +0100

    Debug dust

--- simulations/aura.py
+++ simulations/aura.py
@@ -126,7 +126,7 @@ class Sim(SN_Model):
         args['E'] = E_func(args, self.config['SN_E_model']['params'])
         self.args = args
         args['host_Av'] = host_Av_func(args,self.config['Host_Av_model']['params'])
-        m_av_samples_inds = [[m_av0_samples[i],'%.5f'%(args['host_Av'][i])] for i in range(len(args['host_Av']))]
+        m_av_samples_inds = [[m_samples[i],'%.5f'%(args['host_Av'][i])] for i in range(len(args['host_Av']))]
         gal_df = z_df.loc[m_av_samples_inds]
         args['U-R'] = gal_df['U-R']
         args['mean_ages'] = gal_df['mean_age']
