ef08b459b07bf8a28b9cc9cde57b3e9b1e66e2eb
wisemanp <pacelweb@gmail.com>
2021-06-10 12:18:51 +0100

    Debug array lengths

--- simulations/models/brightness_models.py
+++ simulations/models/brightness_models.py
@@ -31,30 +31,30 @@ def tripp(alpha,beta,M0,sigma_int,mass_step,age_step,args):
     :return:
     :rtype:
     '''
-    return M0 + args['distmod'] + norm(0,sigma_int).rvs(size=len(args['distmod'])) + beta*args['c'] - alpha*args['x1'] +\
+    return M0 + args['distmod'] + norm(0,sigma_int).rvs(size=len(args['c'])) + beta*args['c'] - alpha*args['x1'] +\
            add_mass_step(np.log10(args['mass']),mass_step['mag'],mass_step['loc']) + add_age_step(np.log10(args['age']),age_step['mag'],age_step['loc'])
 
 def tripp_rv(alpha,beta,M0,sigma_int,mass_step,age_step,args):
-    return M0 + args['distmod'] + norm(0,sigma_int).rvs(size=len(args['distmod'])) + beta*args['c_int'] - alpha*args['x1'] + (args['rv']+1)*args['E'] + \
+    return M0 + args['distmod'] + norm(0,sigma_int).rvs(size=len(args['c'])) + beta*args['c_int'] - alpha*args['x1'] + (args['rv']+1)*args['E'] + \
            add_mass_step(np.log10(args['mass']),mass_step['mag'],mass_step['loc']) + add_age_step(np.log10(args['age']),age_step['mag'],age_step['loc'])
 
 def tripp_rv_popn_alpha_beta(mu_alpha,sig_alpha,mu_beta,sig_beta,M0,sigma_int,mass_step,age_step,args):
-    alpha = norm(mu_alpha,sig_alpha).rvs(size=len(args['distmod']))
-    beta = norm(mu_beta, sig_beta).rvs(size=len(args['distmod']))
-    return M0 + args['distmod'] + norm(0,sigma_int).rvs(size=len(args['distmod'])) + beta*args['c_int'] - alpha*args['x1'] + (args['rv']+1)*args['E'] + \
+    alpha = norm(mu_alpha,sig_alpha).rvs(size=len(args['c']))
+    beta = norm(mu_beta, sig_beta).rvs(size=len(args['c']))
+    return M0 + args['distmod'] + norm(0,sigma_int).rvs(size=len(args['c'])) + beta*args['c_int'] - alpha*args['x1'] + (args['rv']+1)*args['E'] + \
            add_mass_step(np.log10(args['mass']),mass_step['mag'],mass_step['loc']) + add_age_step(np.log10(args['age']),age_step['mag'],age_step['loc'])
 
 def tripp_rv_two_beta_age(alpha,beta_young,beta_old,M0,sigma_int,mass_step,age_step,args):
     beta = (beta_old * (args['prog_age']=='old')) + (beta_young * (args['prog_age']=='young'))
-    return M0 + args['distmod'] + norm(0, sigma_int).rvs(size=len(args['distmod'])) + beta * args['c_int'] - alpha * args[
+    return M0 + args['distmod'] + norm(0, sigma_int).rvs(size=len(args['c'])) + beta * args['c_int'] - alpha * args[
         'x1'] + (args['rv'] + 1) * args['E'] + \
            add_mass_step(np.log10(args['mass']), mass_step['mag'], mass_step['loc']) + add_age_step(
         np.log10(args['age']), age_step['mag'], age_step['loc'])
 
 def tripp_rv_two_beta_popns_age(alpha,beta_young,sig_beta_young,beta_old,sig_beta_old,M0,sigma_int,mass_step,age_step,args):
-    beta = (norm(beta_old,sig_beta_old).rvs(size=len(args['distmod'])) * (args['prog_age']=='old')) + (norm(beta_young,sig_beta_young).rvs(size=len(args['distmod'])) * (args['prog_age']=='young'))
+    beta = (norm(beta_old,sig_beta_old).rvs(size=len(args['c'])) * (args['prog_age']=='old')) + (norm(beta_young,sig_beta_young).rvs(size=len(args['c'])) * (args['prog_age']=='young'))
 
-    return M0 + args['distmod'] + norm(0, sigma_int).rvs(size=len(args['distmod'])) + beta * args['c_int'] - alpha * args[
+    return M0 + args['distmod'] + norm(0, sigma_int).rvs(size=len(args['c'])) + beta * args['c_int'] - alpha * args[
         'x1'] + (args['rv'] + 1) * args['E'] + \
            add_mass_step(np.log10(args['mass']), mass_step['mag'], mass_step['loc']) + add_age_step(
         np.log10(args['age']), age_step['mag'], age_step['loc'])
