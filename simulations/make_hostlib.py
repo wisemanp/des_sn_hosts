Fixing PEGASE units

simulations/make_hostlib.py
@@ -118,7 +118,7 @@ def run(args):
                     if args.templates == 'BC03':
                         obs_flux = mtot*fluxes/(distance_factor*bc03_flux_conv_factor)
                     elif args.templates =='PEGASE':
-                        obs_flux = mtot * fluxes*3.828E+33 / (distance_factor)
+                        obs_flux = mtot * fluxes / (distance_factor*3.828E+33)
                     results.append(np.concatenate([[z,mtot,ssfr,mwsa,Av,Rv,delta,U_R[0],pred_rate_x1hi,pred_rate_x1lo,ages,SN_age_dist,pred_rate_total],obs_flux[0]]))
 
     flux_df = pd.DataFrame(results,columns=['z','mass','ssfr','mean_age','Av','Rv','delta','U_R','pred_rate_x1_hi','pred_rate_x1_lo','SN_ages','SN_age_dist','pred_rate_total','f_g','f_r','f_i','f_z'])