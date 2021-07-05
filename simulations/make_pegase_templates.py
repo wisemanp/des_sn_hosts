f6d185b9e5b828c724aebfc5fdcd2cde321a836c
wisemanp <philip@wisemen.co.uk>
2021-07-05 12:32:58 +0100

    Adding save name option

--- simulations/make_pegase_templates.py
+++ simulations/make_pegase_templates.py
@@ -8,8 +8,9 @@ import numpy as np
 def parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-z','--z',help='Redshift',default=0.0,type=str)
-    parser.add_argument('-ne','--neb',action='store_true')
+    parser.add_argument('-ne','--neb',action='store_true')s
     parser.add_argument('-tr','--time_res',help='Time resolution',default=5,type=int)
+    parser.add_argument('-s','--savename',help='Save name',default='new')
     args = parser.parse_args()
     return args
 
@@ -25,7 +26,7 @@ def main(args):
     store = pd.HDFStore('/media/data3/wiseman/des/desdtd/SFHs/SFHs_alt_0.5_Qerf_1.1.h5', 'r')
     ordered_keys = np.sort([int(x.strip('/')) for x in store.keys()])
     scenario_list = [generate_scenario(a,args) for a in ordered_keys[::-1][np.arange(0,len(ordered_keys),args.time_res)]]
-    peg = PEGASE('neb', ssps=SSP(IMF(IMF.IMF_Kroupa), ejecta=SNII_ejecta.MODEL_B, galactic_winds=True),
+    peg = PEGASE(args.savename, ssps=SSP(IMF(IMF.IMF_Kroupa), ejecta=SNII_ejecta.MODEL_B, galactic_winds=True),
                  scenarios=scenario_list)
     peg.generate()
     counter=1
