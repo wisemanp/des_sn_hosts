4ecf4b724193db15ce9d14ee4d4065f89f2a2d15
wisemanp <philip@wisemen.co.uk>
2021-07-07 12:49:50 +0100

    Better SFH interpolation

--- simulations/spectral_utils.py
+++ simulations/spectral_utils.py
@@ -495,13 +495,13 @@ class NebularContinuum(object):
 def interpolate_SFH(sfh,mtot,t_arr):
     '''Function to iterpolate SFHs onto the BC03 age grid '''
     sfh['stellar_age'] = sfh.age.values[::-1]
-    gb =sfh.groupby(pd.cut(sfh['stellar_age'],bins=np.concatenate([[0],10**(t_arr)/(1E+6)]))).agg(sum)
+    gb =sfh.groupby(pd.cut(sfh['stellar_age'],bins=np.concatenate([[0],10**(t_arr)/(1E+6)]))).agg(np.mean)
 
     return gb['m_formed'].values/mtot
 
 
 def interpolate_SFH_pegase(sfh, ages, mtot,marr):
     sfh['stellar_age'] = sfh.age.values[::-1]
-    gb = sfh.groupby(pd.cut(sfh['stellar_age'], bins=np.concatenate([ages, [20001]]))).agg(sum)
+    gb = sfh.groupby(pd.cut(sfh['stellar_age'], bins=np.concatenate([ages, [20001]]))).agg(np.mean)
 
     return gb['m_formed'].values / (mtot)
