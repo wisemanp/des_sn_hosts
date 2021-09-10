2bc823f45cc945756bd90679aa01671e5a2a6073
Philip Wiseman <pacelweb@gmail.com>
2021-09-10 11:32:40 +0100

    Do beta step on SN age

--- simulations/models/brightness_models.py
+++ simulations/models/brightness_models.py
@@ -58,3 +58,12 @@ def tripp_rv_two_beta_popns_age(alpha,mu_beta_young,sig_beta_young,mu_beta_old,s
         'x1']) + (args['rv'] + 1) * args['E'] + \
            add_mass_step(np.log10(args['mass']), mass_step['mag'], mass_step['loc']) + add_age_step(
         args['SN_age'], age_step['mag'], age_step['loc'])
+
+def tripp_rv_two_beta_popns_age2(alpha,mu_beta_young,sig_beta_young,mu_beta_old,sig_beta_old,M0,sigma_int,mass_step,age_step,args):
+    beta = np.array((norm(mu_beta_old,sig_beta_old).rvs(size=len(args['c'])) * (args['SN_age']>age_step['loc'])) + \
+        (norm(mu_beta_young,sig_beta_young).rvs(size=len(args['c'])) * (args['SN_age']<=age_step['loc'])))
+
+    return M0 + args['distmod'] + norm(0, sigma_int).rvs(size=len(args['c'])) + beta * np.array(args['c_int']) - alpha * np.array(args[
+        'x1']) + (args['rv'] + 1) * args['E'] + \
+           add_mass_step(np.log10(args['mass']), mass_step['mag'], mass_step['loc']) + add_age_step(
+        args['SN_age'], age_step['mag'], age_step['loc'])
--- simulations/models/sn_model.py
+++ simulations/models/sn_model.py
@@ -93,3 +93,7 @@ class SN_Model():
     def tripp_rv_two_beta_popns_age(self,args,params):
         return tripp_rv_two_beta_popns_age(params['alpha'], params['mu_beta_young'],params['sig_beta_young'], params['mu_beta_old'], params['sig_beta_old'], params['M0'],
                                      params['sigma_int'], params['mass_step'], params['age_step'], args)
+
+    def tripp_rv_two_beta_popns_age2(self,args,params):
+        return tripp_rv_two_beta_popns_age(params['alpha'], params['mu_beta_young'],params['sig_beta_young'], params['mu_beta_old'], params['sig_beta_old'], params['M0'],
+                                     params['sigma_int'], params['mass_step'], params['age_step'],args)
