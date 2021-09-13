77c0e6d591a67362f53b8dc5eeab3b55dd8546f8
Philip Wiseman <pacelweb@gmail.com>
2021-09-13 13:19:03 +0100

    Do dropna in place

--- simulations/aura.py
+++ simulations/aura.py
@@ -128,21 +128,16 @@ class Sim(SN_Model):
         time.sleep(2)
         args['n'] = int(n_samples)
         args['distmod'] = self.cosmo.distmod(z).value
-        print('here 1')
-        time.sleep(2)
+
         z_df = self.multi_df.loc['%.2f' % z]
-        print('here 2')
-        time.sleep(2)
+
         z_df['N_total'].replace(0., np.NaN, inplace=True)
-        z_df = z_df.dropna(subset=['N_total'])
-        print('here 3')
-        time.sleep(2)
+        z_df.dropna(subset=['N_total'],inplace=True)
+
         z_df['N_SN_float'] = z_df['N_total'] / z_df['N_total'].min()  # Normalise the number of SNe so that the most improbable galaxy gets 1
-        print('here 4')
-        time.sleep(2)
+
         z_df['N_SN_int'] = z_df['N_SN_float'].astype(int)
-        print('here 5')
-        time.sleep(2)
+
         # Now we set up some index arrays so that we can sample masses properly
         m_inds = ['%.2f' % m for m in z_df['mass'].unique()]
         m_rates = []
