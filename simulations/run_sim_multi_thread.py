54488de35c5a3d3f0179159e4f209b67da02c0c5
phil <philip@wisemen.co.uk>
2021-09-29 13:29:16 +0100

    Add BBC convertion to general script

--- simulations/run_sim_multi_thread.py
+++ simulations/run_sim_multi_thread.py
@@ -6,12 +6,55 @@ from tqdm import tqdm
 import sys
 from yaml import safe_load as yload
 from yaml import safe_dump as ydump
+import csv
+import numpy as np
 
 import multiprocessing
 
 def load_config(cpath):
     with open(cpath,'r') as f:
         return yload(f)
+
+def prep_df_for_BBC(df):
+    df = df[df['mB']<23.35]
+    df = df[(df['x1']<3)&(df['x1']>-3)&(df['c']>-0.3)&(df['c']<0.3)&\
+                           (df['x1_err']<1)&(df['c_err']<0.05)]
+    df['CID'] = np.arange(len(df),dtype=int)
+    #df['CID'] = df['CID'].astype(int)
+    df['IDSURVEY'] = 10
+    df['TYPE'] = 101
+    df.rename(columns={'z':'zHD','mB_err':'mBERR','x1_err':'x1ERR','c_err':'cERR',
+                             },inplace=True)
+    df = df[df['zHD']>0.15]
+    df['zHDERR'] = 0.0001
+    df['zCMB'] = df['zHD']
+    df['zCMBERR'] = 0.0001
+    df['zHEL'] = df['zHD']
+    df['zHELERR'] = 0.0001
+    df['VPEC'] =0
+    df['VPECERR'] =0
+    df['x0']=10**(-0.4*(df['mB']-10.6350))
+    df['x0ERR']=0.4*np.log(10)*df['x0'].values*df['mBERR']
+    df['COV_x1_x0'] =0
+    df['COV_c_x0'] =0
+    df['COV_x1_c'] =0
+    df['PKMJD'] =56600
+    df['PKMJDERR'] =0.1
+    df['FITPROB'] =1
+    df['PROB_SNNTRAINV19_z_TRAINDES_V19']=1
+    df['HOST_LOGMASS'] = np.log10(df['mass'])
+    df['HOST_LOG_SFR'] = np.log10(df['sfr'])
+    df['HOST_LOG_sSFR'] = np.log10(df['ssfr'])
+    df['VARNAMES:'] = 'SN:'
+    columns=['VARNAMES:','CID', 'IDSURVEY', 'TYPE', 'mB', 'mBERR', 'cERR', 'x1ERR', 'zHD', 'TYPE', 'zHDERR', 'zCMB',
+           'zCMBERR', 'zHEL', 'zHELERR','x0', 'x0ERR','COV_x1_x0', 'COV_c_x0', 'COV_x1_c',
+             'VPEC', 'VPECERR', 'PKMJD', 'PKMJDERR', 'FITPROB',
+           'PROB_SNNTRAINV19_z_TRAINDES_V19', 'HOST_LOGMASS', 'HOST_LOG_SFR',
+           'HOST_LOG_sSFR', 'distmod', 'mass', 'ssfr', 'sfr', 'mean_ages',
+           'SN_age', 'rv', 'E', 'host_Av', 'U-R', 'c', 'e_noise', 'c_int', 'x1',
+           #'prog_age'
+            ]
+    return df,columns
 
 def sim_worker(args):
 
@@ -39,6 +82,7 @@ def sim_worker(args):
     sim.sim_df = sim.sim_df[(sim.sim_df['x1']<3)&(sim.sim_df['x1']>-3)&(sim.sim_df['c']>-0.3)&\
                             (sim.sim_df['c']<0.3)&(sim.sim_df['x1_err']<1)&(sim.sim_df['c_err']<0.05)]
     sim.sim_df = sim.sim_df[sim.sim_df['mB']<23.35]
+
     sim.sim_df.to_hdf(os.path.join('/media/data3/wiseman/des/AURA/sims/SNe/for_BBC/',cfg['save']['dir'],
         '%s_test_SN_sim_%.2f_%.2f_%.2f.h5'%(model_name,rv_lo,rv_hi,age_step)),key='sim')
 
@@ -66,3 +110,22 @@ if __name__=='__main__':
             for age_step in age_step_grid:
                 args.append([rv_hi,rv_lo,age_step,cfg])
     multi_sim(args)
+    print('Simulations complete. Now converting to BBC format')
+    n=0
+    param_map = {}
+    pth = cfg['config_path']
+    model_config = os.path.split(pth)[-1]
+    model_name = model_config.split('.')[0]
+    for rv_lo in Rv_lo_grid:
+        for rv_hi in Rv_hi_grid:
+            for age_step in age_step_grid:
+                df = pd.read_hdf(os.path.join('/media/data3/wiseman/des/AURA/sims/SNe/for_BBC/',cfg['save']['dir'],
+                    '%s_test_SN_sim_%.2f_%.2f_%.2f.h5'%(model_name,rv_lo,rv_hi,age_step)))
+                df,cols = prep_df_for_BBC(df)
+                df[cols].to_csv(os.path.join('/media/data3/wiseman/des/AURA/sims/SNe/for_BBC/',
+                        cfg['save']['dir'],'FITOPT%03d.FITRES'%n),
+                    index=False, sep=' ', quoting=csv.QUOTE_NONE, quotechar="",escapechar=" ")
+                param_map[n] = [rv_lo,rv_hi,age_step]
+                n+=1
+    pickle.dump(param_map,
+        open('/media/data3/wiseman/des/AURA/sims/SNe/for_BBC/%s/param_name_map.pkl'%(cfg['save']['dir']),'wb'))
