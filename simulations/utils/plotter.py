d2e943d520e695df879b7ef3e7d5d0f366a36257
phil <philip@wisemen.co.uk>
2022-01-04 14:49:12 +0000

    Allow option to not plot DES5yr on x1 fig

--- simulations/utils/plotter.py
+++ simulations/utils/plotter.py
@@ -187,25 +187,27 @@ def plot_cs(sim,df,des5yr,):
     yplus= intervals[:,1] -counts
     chi2 = get_red_chisq(counts,simcounts,yplus)
     return chi2
-def plot_x1s(sim,df,des5yr,scatter_all=False,f=None,ax1=None,ax2=None,nplot=0,sim_colour='c',model_name='Sim',sim_linestyle='-'):
+def plot_x1s(sim,df,des5yr,scatter_all=False,f=None,ax1=None,ax2=None,nplot=0,sim_colour='c',model_name='Sim',sim_linestyle='-',plot_des=True):
 
     if f ==None:
         f,(ax1,ax2)=plt.subplots(1,2,figsize=(12,6.5),sharey=True)
     df['logmass'] = np.log10(df['mass'])
     lcounter=0
-    for n,g in des5yr.groupby(pd.cut(des5yr['massmc'],bins=np.linspace(8,12,30))):
-        if len(g)>1 and nplot==0:
-            label=None
-            if lcounter==0:
-                label = 'DES5YR mean'
-                lcounter+=1
-            ax1.scatter(n.mid,g['x1'].mean(),color=data_colour,edgecolor='grey',linewidth=1,marker='s',s=100,label=label)
-            ax1.errorbar(n.mid,g['x1'].mean(),yerr=np.std(g['x1'])/np.sqrt(len(g['x1'])),c=data_colour,marker=None,ls='none')
+    if plot_des:
 
-        elif len(g)==1 and nplot==0:
+        for n,g in des5yr.groupby(pd.cut(des5yr['massmc'],bins=np.linspace(8,12,30))):
+            if len(g)>1 and nplot==0:
+                label=None
+                if lcounter==0:
+                    label = 'DES5YR mean'
+                    lcounter+=1
+                ax1.scatter(n.mid,g['x1'].mean(),color=data_colour,edgecolor='grey',linewidth=1,marker='s',s=100,label=label)
+                ax1.errorbar(n.mid,g['x1'].mean(),yerr=np.std(g['x1'])/np.sqrt(len(g['x1'])),c=data_colour,marker=None,ls='none')
 
-            ax2.scatter(n.mid,g['x1'].mean(),color=data_colour,edgecolor='grey',linewidth=1,marker='s',s=100)
-            ax2.errorbar(n.mid,g['x1'].mean(),yerr=g['x1ERR'],c=data_colour,marker=None,ls='none')
+            elif len(g)==1 and nplot==0:
+
+                ax2.scatter(n.mid,g['x1'].mean(),color=data_colour,edgecolor='grey',linewidth=1,marker='s',s=100)
+                ax2.errorbar(n.mid,g['x1'].mean(),yerr=g['x1ERR'],c=data_colour,marker=None,ls='none')
     if scatter_all:
         ax1.scatter(df['logmass'],df['x1'],alpha=0.4,edgecolor='w',lw=0.1,label='Sim')
 
