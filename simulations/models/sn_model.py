c4602b580e786f04e22ca235584023ed6d70c88c
phil <philip@wisemen.co.uk>
2022-01-17 11:06:49 +0000

    Add params to x1 worker function

--- simulations/models/sn_model.py
+++ simulations/models/sn_model.py
@@ -50,7 +50,7 @@ class SN_Model():
         return args
 
     def x1_int_linear_gauss(self,args,params):
-        args['x1'] = x1_int_linear_gauss(args['SN_age'],args['slope'],)
+        args['x1'] = x1_int_linear_gauss(args['SN_age'],params['slope'],)
         args['prog_age'] = 'Young'
         return args
 
