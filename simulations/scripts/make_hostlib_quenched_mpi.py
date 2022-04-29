1298aa85509c011b5e3ef6781c4896f4a3fb35a2
phil <philip@wisemen.co.uk>
2022-04-29 15:58:17 +0100

    Debug returning

--- simulations/scripts/make_hostlib_quenched_mpi.py
+++ simulations/scripts/make_hostlib_quenched_mpi.py
@@ -112,7 +112,7 @@ def sed_worker(worker_args):
         df.to_hdf('/media/data3/wiseman/des/AURA/sims/hostlibs/all_model_params_quench_%s_z%.2f_%.2f_av%.2f_%.2f_rv_rand_full_age_dists_neb_U%.2f_res_%i_beta_%.2f.h5'%(args.templates,args.zlo,args.zhi,av_arr[0],av_arr[-1],args.logU,args.time_res,args.beta),
             key='%.2f/%i'%(z,tf))
     except:
-        pass
+        return
 
     return
 
