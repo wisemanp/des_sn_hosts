437fc5dafd710770442339c0da0111c7b10c0389
phil <philip@wisemen.co.uk>
2022-08-10 15:25:32 +0100

    Lower redshift range

--- simulations/scripts/get_gammas.py
+++ simulations/scripts/get_gammas.py
@@ -112,7 +112,7 @@ n_samples=config['n_samples']
 ## Do the simulations
 highz_fn = '/media/data3/wiseman/des/AURA/sims/SNe/Briday/DES_BS20_age_Rv_step_3Gyr_age_x1_beta_1.14_quenched_bursty_highz_%i_SN_sim.h5'%config['simno']
 lowz_fn = highz_fn.replace('high','low')
-zarr_highz = np.arange(0.14,1.2,0.1)
+zarr_highz = np.arange(0.14,0.5,0.05)
 zarr_lowz = np.arange(0.0105,0.14,0.01)
 zarr = np.concatenate([zarr_lowz,zarr_highz])
 if do_sim:
@@ -125,7 +125,7 @@ if do_sim:
     n_samples_arr = sim._get_z_dist(numbers,n=n_samples,frac_low_z=0,zbins=zarr_lowz+0.005)
     print('Lowz with these samples',zarr_lowz,n_samples_arr)
     sim.sample_SNe(zarr_lowz,n_samples_arr,savepath=lowz_fn)
-    zs = np.linspace(0.14,1.2,1000)
+    zs = np.linspace(0.14,0.5,1000)
     zs_cubed = zs_cubed = np.ones_like(zs)#zs**3.
     numbers = np.random.choice(zs,p=zs_cubed/np.sum(zs_cubed),size=n_samples)
     n_samples_arr = sim._get_z_dist(numbers,n=n_samples,frac_low_z=0,zbins=zarr_highz+0.02)
