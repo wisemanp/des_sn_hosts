bfd9534d54ad5d41018fdbcc46da34ed53a39c20
phil <philip@wisemen.co.uk>
2022-10-19 14:45:33 +0100

    Add SPECID_GAL

--- simulations/scripts/make_hostlib_snana.py
+++ simulations/scripts/make_hostlib_snana.py
@@ -135,7 +135,7 @@ for z in tqdm(sim.multi_df.z.unique()):
             new_zdf['SN_age_dist']=age_dists
             # Now we sample from our galaxy mass distribution, given the expected rate of SNe at each galaxy mass
 
-            gals_df = new_zdf.loc[m_av0_samples,['z','mass','ssfr','m_g','m_r','m_i','m_z','U', 'B', 'V', 'R', 'I','U_R','mean_age','Av','pred_rate_total','sdssu','sdssg','sdssr','sdssi','sdssz']]
+            gals_df = new_zdf.loc[m_av0_samples,['z','mass','ssfr','m_g','m_r','m_i','m_z','U', 'B', 'V', 'R', 'I','U_R','mean_age','Av','pred_rate_total','sdssu','sdssg','sdssr','sdssi','sdssz','galid_spec']]
 
             sn_ages = [np.random.choice(new_zdf.loc[i,'SN_ages'],p=new_zdf.loc[i,'SN_age_dist']) for i in m_av0_samples] #/new_zdf.loc[i,'SN_age_dist'].sum()
             gals_df['SN_age'] = np.array(sn_ages)
@@ -166,7 +166,7 @@ hostlib_df['VARNAMES:'] = 'GAL'
 hostlib_df.reset_index(inplace=True)
 hostlib_df.reset_index(inplace=True)
 
-hostlib_df.rename(columns={'index:':'GALID','index':'GALID','m_g':'g_obs','m_r':'r_obs','m_i':'i_obs','m_z':'z_obs'},inplace=True)
+hostlib_df.rename(columns={'index:':'GALID','index':'GALID','m_g':'g_obs','m_r':'r_obs','m_i':'i_obs','m_z':'z_obs','galid_spec':'SPECID_GAL'},inplace=True)
 
 
 field_centres = np.array([[54.2743, -27.1116],
@@ -193,6 +193,6 @@ hostlib_df=hostlib_df[['VARNAMES:','GALID', 'RA','DEC','ZTRUE', 'g_obs', 'r_obs'
             'n0_Sersic','a0_Sersic', 'b0_Sersic', 'a_rot',
             'LOGMASS','LOGMASS_ERR','LOG_SFR', 'LOG_SFR_ERR', 'LOG_sSFR','obs_gr',
        'U', 'B', 'V', 'R', 'I', 'U_R', 'mean_age', 'Av', 'pred_rate_total',
-       'SN_age','sdssu','sdssg','sdssr','sdssi','sdssz']]
+       'SN_age','sdssu','sdssg','sdssr','sdssi','sdssz','SPECID_GAL']]
 hostlib_df.to_hdf(os.path.join(savedir,'%s_combined.h5'%sys.argv[2]),key='main',index=False)
 hostlib_df.to_csv(os.path.join(savedir,'%s_combined.csv'%sys.argv[2]),)
