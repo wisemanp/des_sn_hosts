e4637d426892d7dbbdda4f9257d1976408ee96bb
phil <philip@wisemen.co.uk>
2025-11-07 13:59:02 +0000

    debug scatter

--- simulations/aura.py
+++ simulations/aura.py
@@ -590,7 +590,7 @@ class Sim(SN_Model):
         args = self.colour_func(args, self.config['SN_colour_model']['params'])
         args = self.x1_func(args, self.config['x1_model']['params'])
         args['mB'], args['alpha_SN'], args['beta_SN'] = self.mb_func(args, self.config['mB_model']['params'])
-
+        args['mB_true'] = np.asarray(args['mB']).copy()  # debug: store model output
         # Errors and noise
         args['mB_err'] = [
             np.max([0.025, np.random.normal(10**(0.395*(mB-1.5) - 10.15) + 0.025,
@@ -602,7 +602,7 @@ class Sim(SN_Model):
         args['c_noise'] = stats.norm(0, args['c_err']).rvs(size=len(args['c']))
         if not bool(self.config.get('c_perfect', False)):
             args['c'] = args['c'] + args['c_noise']
-
+            print('Adding colour noise')
         args['x1_err'] = [np.max([0.08, np.random.normal((11.525*err - 0.1075), 0.05)])
                           for err in args['mB_err']]
         args['x1_noise'] = stats.norm(0, args['x1_err']).rvs(size=len(args['x1']))
